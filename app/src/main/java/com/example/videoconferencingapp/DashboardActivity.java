c2067352af1be09c823630626dedad6974207f28
Nirupam-paul <58510626+Nirupam-paul@users.noreply.github.com>
2021-08-21 19:55:02 +0530

    Redesign of app

--- app/src/main/java/com/example/videoconferencingapp/DashboardActivity.java
+++ app/src/main/java/com/example/videoconferencingapp/DashboardActivity.java
@@ -2,10 +2,13 @@ package com.example.videoconferencingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import org.jitsi.meet.sdk.JitsiMeet;
 import org.jitsi.meet.sdk.JitsiMeetActivity;
@@ -13,21 +16,23 @@ import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Random;
 
 public class DashboardActivity extends AppCompatActivity {
 
     EditText secretCodeBox;
-    Button joinBtn, shareBtn ;
+    Button joinBtn, shareBtn, createBtn ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-
         secretCodeBox = findViewById(R.id.codeBox);
         joinBtn = findViewById(R.id.joinBtn);
         shareBtn = findViewById(R.id.shareBtn);
+        createBtn = findViewById(R.id.createBtn);
 
         URL serverURL;
 
@@ -59,5 +64,35 @@ public class DashboardActivity extends AppCompatActivity {
             }
         });
 
+        createBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                Random rnd = new Random();
+                StringBuilder sb = new StringBuilder(6);
+                for (int i = 0; i < 6; i++)
+                    sb.append(chars.charAt(rnd.nextInt(chars.length())));
+                secretCodeBox.setText(sb);
+            }
+        });
+
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String code = secretCodeBox.getText().toString();
+                if (code.length() < 6 || code.length() > 6 ) {
+                    Toast.makeText(getApplicationContext(),"Your 6 digit Code is not valid!",Toast.LENGTH_SHORT).show();
+                }
+                else if (code.length() == 6 ) {
+                    Intent shareIntent =   new Intent(android.content.Intent.ACTION_SEND);
+                    shareIntent.setType("text/plain");
+                    shareIntent.putExtra(Intent.EXTRA_SUBJECT," From Connect Kora");
+                    shareIntent.putExtra(android.content.Intent.EXTRA_TEXT,"Connect Kora \n Join code: "+ code);
+                    startActivity(Intent.createChooser(shareIntent, "Share via "));
+                }
+
+            }
+        });
     }
 }
